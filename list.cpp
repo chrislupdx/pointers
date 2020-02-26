88ea7ba85dbe85307a868caee8f9ab877625c2a3
Lu <chris.lu@syccuxas01.pcc.edu>
2020-02-25 17:37:32 -0800

    cleaned. far from the done

--- list.cpp
+++ list.cpp
@@ -12,11 +12,6 @@ list::list(int size_int)
 {
   size = 0;
   data = new char * [size_int];
-  //char ** data = new char* [size_int]; //allocate space the size of the incoming int
-  //for(int idx = 0; idx < (size_int - 1);  idx++)
-  //{
-  //  data[idx] = nullptr; //set each
-  //}
 }
 
 void list::insert(const char *phrase) //adds c strings to the list
@@ -36,27 +31,10 @@ void list::insert(const char *phrase) //adds c strings to the list
     delete [] data; //data is 
     data = new_data;
   }
-
-  //why don't we increment size immediately upon call?
-  //int string_length = strlen(phrase); //know the size of a row
-  //char ** temp = new char *[string_length + 1];       //make the array of envp values  +1 bigger
-  //for(int idx = 0; idx < size; size++) temp[idx] = nullptr; //assign all values to null (so they're not junk)
-  //for(int idx = 0; idx < size; idx++) temp[idx] = data[idx]; //write data -> temp
-  //for(int idx = 0; idx < size; idx++) data[idx] = new char[string_length]; //make the strings bigger, which should empty everything //we could make it leave alone ones already good
-  //for(int idx = 0; idx < size; idx++) data[idx] = temp[idx]; //put everything back into the improved array
-  //if(data[0] != nullptr){
-  //if((data[size][0] < 32) |(data[size][0] > 127)) //if there are no printable characters at point of insertion
-  //{
-  //for(int idx = 0; idx < (string_length - 1); idx++) data[size][idx] = phrase[idx]; //copy phrase into your new pointer
-  //}
-//}
-//size++;
 }
 
 std::ostream & operator << (std::ostream &ugh, list &list_class)
 {
-  //std::cout << list_class.size << std::endl;
-  //for(int idx = 0; idx < list_class.size; idx++) std::cout << list_class.data[idx];
   for(int idx = 0; idx < list_class.size; idx++)
   {
     ugh << list_class.data[idx] << std::endl;
